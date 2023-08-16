a1a0e09df1e30b704b05b70f7911614aee20496c
ungHeung <t01047377477@gmail.com>
2023-08-16 23:32:20 +0900

    design : 인풋 컴포넌트 스타일 적용

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -4,7 +4,62 @@
 
 import React from "react";
 import { InputProps } from "../../../libs/interface/CommonInterface";
+import { styled } from "styled-components";
 
 export const Input = ({ ...props }: InputProps) => {
-  return <input type={props.type} placeholder={props.placeholder} />;
+  return (
+    <ButtonContainer>
+      <StyledInput {...props} />
+      <StyledLabel htmlFor={props.id}>{props.label}</StyledLabel>
+    </ButtonContainer>
+  );
 };
+
+const ButtonContainer = styled.div`
+  position: relative;
+`;
+
+const StyledLabel = styled.label`
+  position: absolute;
+  top: 0;
+  left: 10px;
+  display: block;
+  width: 240px;
+  height: 50px;
+  font-family: var(--font-eng);
+  font-size: var(--fsize-l);
+  font-style: italic;
+  line-height: 50px;
+  color: var(--color-gray-d);
+  cursor: text;
+  transition: top, height, font 0.3s, 0.3s;
+`;
+
+const StyledInput = styled.input<InputProps>`
+  width: 240px;
+  height: 1px;
+  margin-top: 49px;
+  box-sizing: border-box;
+  border-bottom: solid 1px var(--color-main);
+  vertical-align: middle;
+  color: white;
+  text-align: center;
+  transition: margin, height 0.3s, 0.3s;
+
+  &:focus,
+  &:not(:placeholder-shown) {
+    height: 50px;
+    margin-top: 0;
+    background-color: var(--color-main);
+    border-radius: 5px;
+  }
+
+  &:focus + ${StyledLabel}, &:not(:placeholder-shown) + ${StyledLabel} {
+    top: -20px;
+    left: 0px;
+    height: 70px;
+    font-size: var(--font-size-s);
+    line-height: 20px;
+    z-index: -1;
+  }
+`;
