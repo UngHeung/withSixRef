2219b9719a411e1c74a5512839d2b805011b307d
ungHeung <t01047377477@gmail.com>
2023-08-16 21:57:44 +0900

    feat : 공통 버튼, 인풋 타입 및 이벤트 타입 추가

---
+++ src/libs/type/CommonType.ts
@@ -0,0 +1,9 @@
+/**
+ * 공용 타입
+ */
+
+export type LAYOUT_TYPE_VARIANTS = "primary" | "secondary";
+
+export type BUTTON_TYPE_VARIANTS = "basic" | "follow";
+
+export type INPUT_TYPE_VARIANTS = "basic" | "search";
