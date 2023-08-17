4b052b3d680f6427a86c5b88639fe60c192f161e
ungHeung <t01047377477@gmail.com>
2023-08-17 18:18:36 +0900

    feat : 푸터 nav메뉴 추가

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { styled } from "styled-components";
+import { Nav } from "../nav/Nav";
 
 export const Footer = () => {
   return (
-    <footer>
-      <></>
-    </footer>
+    <StyledFooter>
+      <Nav />
+    </StyledFooter>
   );
 };
+
+const StyledFooter = styled.footer`
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  width: 390px;
+  height: 50px;
+  margin: 0 auto;
+  transform: translateX(-50%);
+`;
