01a2007e6a7bed13695a4e0b7d45c64d0a79cc6c
ungHeung <t01047377477@gmail.com>
2023-08-17 16:29:03 +0900

    feat : 레이아웃 적용

--- src/pages/sign/SignIn.tsx
+++ src/pages/sign/SignIn.tsx
@@ -8,20 +8,21 @@ import { buttonOptions, inputOptions } from "./props";
 import { Input } from "../../components/common/input/Input";
 import handler from "./handler";
 import { INPUT_EVENT } from "../../libs/type/EventType";
+import { Layout } from "../../layout/Layout";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <>
+    <Layout typeVariants="secondary">
       <section>top image</section>
       <section>
         <Input {...inputOptions.email} value={email} onChange={(event: INPUT_EVENT) => handler.email(setEmail, event)} />
         <Input {...inputOptions.password} value={password} onChange={(event: INPUT_EVENT) => handler.email(setPassword, event)} />
         <Button {...buttonOptions}>로그인</Button>
       </section>
-    </>
+    </Layout>
   );
 };
 
