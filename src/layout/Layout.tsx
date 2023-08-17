bc861adca244e19fed397cae97bcf0b8f0b35bbf
ungHeung <t01047377477@gmail.com>
2023-08-17 16:23:18 +0900

    feat : 헤더, 푸터 위치 추가, 표시 미표시 속성 추가

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,12 +4,23 @@
 
 import { styled } from "styled-components";
 import { LayoutProps } from "../libs/interface/CommonInterface";
+import { Header } from "./header/Header";
+import { Footer } from "./footer/Footer";
 
 export const Layout = ({ ...props }: LayoutProps) => {
-  return <StyleMain>{props.children}</StyleMain>;
+  return (
+    <>
+      {!props.disabled && <Header />}
+      <StyledMain>{props.children}</StyledMain>
+      {!props.disabled && <Footer />}
+    </>
+  );
 };
 
-const StyleMain = styled.main`
+// const StyledBack = styled.div
+
+const StyledMain = styled.main`
   width: 390px;
+  margin: 0 auto;
   background-color: white;
 `;
