80eb63a432b9d7bb70500ce6421a9194bb475b51
ungHeung <t01047377477@gmail.com>
2023-08-17 18:41:49 +0900

    design : 헤더 로고, 유저이름 스타일등 추가

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,9 +1,54 @@
 import React from "react";
+import { styled } from "styled-components";
+import logo from "../../assets/logo/LOGO-negative.svg";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header>
-      <></>
-    </header>
+    <StyledHeader>
+      <StyledLogo>
+        <Link to="/">
+          <img src={logo} alt="로고" />
+        </Link>
+      </StyledLogo>
+      <StyledUserName>엉흥</StyledUserName>
+    </StyledHeader>
   );
 };
+
+const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 50%;
+  width: 390px;
+  height: 70px;
+  padding: 0 20px;
+  background-color: var(--color-main);
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  box-sizing: border-box;
+  line-height: 70px;
+  transform: translateX(-50%);
+  overflow: hidden;
+
+  & img {
+    width: 50px;
+    vertical-align: middle;
+  }
+`;
+
+const StyledLogo = styled.h2`
+  float: left;
+  line-height: 65px;
+`;
+
+const StyledUserName = styled.span`
+  float: right;
+  color: white;
+
+  &::after {
+    content: "님";
+    margin-left: 2px;
+    font-size: var(--font-size-s);
+  }
+`;
