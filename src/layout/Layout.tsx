e20a1a0b057c233ffae44deb485f9d89085825a5
ungHeung <t01047377477@gmail.com>
2023-08-17 18:53:28 +0900

    feat : 레이아웃 타입에 따른 padding 속성값 변경

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,7 +2,7 @@
  * 애플리케이션 공용 레이아웃
  */
 
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 import { LayoutProps } from "../libs/interface/CommonInterface";
 import { Header } from "./header/Header";
 import { Footer } from "./footer/Footer";
@@ -11,7 +11,7 @@ export const Layout = ({ ...props }: LayoutProps) => {
   return (
     <>
       {props.typeVariants === "primary" && <Header />}
-      <StyledMain>{props.children}</StyledMain>
+      <StyledMain {...props}>{props.children}</StyledMain>
       {props.typeVariants === "primary" && <Footer />}
     </>
   );
@@ -19,8 +19,22 @@ export const Layout = ({ ...props }: LayoutProps) => {
 
 // const StyledBack = styled.div
 
-const StyledMain = styled.main`
+const StyledMain = styled.main<LayoutProps>`
+  ${(props) => TYPE_VARIANTS[props.typeVariants]}
+
   width: 390px;
+  height: 100vh;
   margin: 0 auto;
   background-color: white;
+  border: 1px solid var(--color-gray-l);
+  box-sizing: border-box;
 `;
+
+const TYPE_VARIANTS = {
+  primary: css`
+    padding: 70px 20px 50px;
+  `,
+  secondary: css`
+    padding: 0 20px;
+  `,
+};
