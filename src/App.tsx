8c35cc640ee46c18cd5bb81fb8685eadfbe86889
ungHeung <t01047377477@gmail.com>
2023-08-18 14:06:50 +0900

    chore : 라우트 home 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import SignIn from "./pages/sign/SignIn";
+import { Home } from "./pages/home/Home";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <BrowserRouter>
         <GlobalStyle />
         <Routes>
-          <Route path="/" element={<SignIn />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
         </Routes>
       </BrowserRouter>
     </>
